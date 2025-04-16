4ac4feb7a47333cbb16c73bbd79327a10bf6ccc4
Cooperativebuilders <admin@cooperativebuilders.com>
2025-04-16 11:41:35 +0100

    moved guidance link

--- src/components/DashboardSidebar.js
+++ src/components/DashboardSidebar.js
@@ -40,6 +40,9 @@ const DashboardSidebar = ({ onLogout }) => {
         <Link to="/projects" className={linkClass("/projects")}>
           <FaProjectDiagram /> <span>View Projects</span>
         </Link>
+        <Link to="/guidance" className={linkClass("/guidance")}>
+          <FaBook /> <span>Guidance</span>
+        </Link>
         <button
           onClick={onLogout}
           className="flex items-center space-x-2 p-3 w-full text-left text-red-600 hover:bg-red-100 transition rounded-md"
@@ -47,9 +50,6 @@ const DashboardSidebar = ({ onLogout }) => {
           <FaSignOutAlt /> <span>Logout</span>
         </button>
       </nav>
-      <Link to="/guidance" className={linkClass("/guidance")}>
-        <FaBook /> <span>Guidance</span>
-      </Link>
     </aside>
   );
 };
