12cba07aa98db479fc5fca26641a68ffbd665a68
Cooperativebuilders <admin@cooperativebuilders.com>
2025-04-16 12:29:23 +0100

    2

--- src/components/DashboardSidebar.js
+++ src/components/DashboardSidebar.js
@@ -41,7 +41,7 @@ const DashboardSidebar = ({ onLogout }) => {
           <FaProjectDiagram /> <span>View Projects</span>
         </Link>
         <Link to="/guidance" className={linkClass("/guidance")}>
-          <FaBook /> <span>Guidanc Notes</span>
+          <FaBook /> <span>Guidance Notes</span>
         </Link>
         <button
           onClick={onLogout}
