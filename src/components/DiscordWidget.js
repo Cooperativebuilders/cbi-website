39fd7d7d05097efe322f429d559f15bcb56a2974
Cooperativebuilders <admin@cooperativebuilders.com>
2025-04-17 20:53:29 +0100

    2

--- src/components/DiscordWidget.js
+++ src/components/DiscordWidget.js
@@ -5,14 +5,13 @@ const DiscordWidget = () => {
   // Replace DISCORD_SERVER_ID with your actual server ID
   return (
     <iframe
-      title="Discord Widget"
       src="https://discord.com/widget?id=1355137205445591100&theme=dark"
       width="350"
       height="500"
-      allowTransparency="true"
-      frameBorder="0"
+      allowtransparency="true"
+      frameborder="0"
       sandbox="allow-popups allow-popups-to-escape-sandbox allow-same-origin allow-scripts"
-    />
+    ></iframe>
   );
 };
 
