935db1d93dbf7addf22fb11d0ce59cfcee234c9a
Cooperativebuilders <admin@cooperativebuilders.com>
2025-04-17 12:31:03 +0100

    2

--- src/components/ProjectTile.js
+++ src/components/ProjectTile.js
@@ -1,5 +1,4 @@
 // src/components/ProjectTile.js
-
 import React from "react";
 import {
   doc,
@@ -96,6 +95,15 @@ function ProjectTile({ project, projectId }) {
 
   return (
     <div className="bg-white shadow-md rounded-lg p-4 relative">
+      {/* Show image if available */}
+      {project.imageUrl && project.imageUrl !== "" && (
+        <img
+          src={project.imageUrl}
+          alt="Project"
+          className="w-full h-auto object-cover rounded mb-2"
+        />
+      )}
+
       <h3 className="text-xl font-bold text-blue-700 mb-2">
         {project.location}
       </h3>
